d6c3b3f3a5debe844347c8d66acd519631de047a
Felipe <andresfppy@gmail.com>
2024-01-29 18:42:51 -0500

    first commit

---
+++ src/main/java/com/app/reservasR/ReservasRApplication.java
@@ -0,0 +1,13 @@
+package com.app.reservasR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasRApplication.class, args);
+	}
+
+}
