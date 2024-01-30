f54b11ca98f85a4916c5cb8e59bf64284864892b
Felipe <andresfppy@gmail.com>
2024-01-30 14:17:13 -0500

    Agregar record DiningTableDto

---
+++ src/main/java/com/app/reservasR/domain/dto/DiningTableDto.java
@@ -0,0 +1,11 @@
+package com.app.reservasR.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record DiningTableDto(
+        Integer Id,
+        int capacidad
+) {
+
+}
