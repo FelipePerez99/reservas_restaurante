994b091685c3a2baa7b62ee24498eb0053c14b19
Felipe <andresfppy@gmail.com>
2024-01-30 19:20:57 -0500

    Agregar metodos a record DiningTableService con su implementación

--- src/main/java/com/app/reservasR/application/service/DiningTableService.java
+++ src/main/java/com/app/reservasR/application/service/DiningTableService.java
@@ -3,11 +3,14 @@ package com.app.reservasR.application.service;
 import com.app.reservasR.application.Exception.EntityNotFoundException;
 import com.app.reservasR.application.lasting.EMessage;
 import com.app.reservasR.domain.dto.DiningTableDto;
+import com.app.reservasR.domain.dto.UserDto;
 import com.app.reservasR.domain.entity.DiningTable;
+import com.app.reservasR.domain.entity.User;
 import com.app.reservasR.domain.repository.DiningTableRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,5 +38,23 @@ public record DiningTableService(DiningTableRepository diningTableRepository) {
         List<DiningTable> diningTables = diningTableRepository.findAll();
         return diningTables.stream().map(diningTable -> new DiningTableDto(diningTable.getId(),
                 diningTable.getCapacity())).collect(Collectors.toList());
+    }
+
+    public DiningTableDto updateDiningTable(Integer id, DiningTableDto diningTableDto) throws EntityNotFoundException{
+        DiningTable diningTable = diningTableRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(EMessage.DATA_NOT_FOUND));
+
+        diningTable.setCapacity(diningTableDto.capacity());
+
+        DiningTable updateDiningTable = diningTableRepository.save(diningTable);
+        return new DiningTableDto(updateDiningTable.getId(), updateDiningTable.getCapacity());
+    }
+
+    public void deleteById(Integer id)throws EntityNotFoundException {
+        Optional<DiningTable> diningTable = diningTableRepository.findById(id);
+        if (diningTable.isPresent()){
+            diningTableRepository.deleteById(id);
+        }else{
+            throw new EntityNotFoundException(EMessage.DATA_NOT_FOUND);
+        }
     }
 }
