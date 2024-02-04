9a1746371ae87771c72b9727949ed767ab64d1a1
Felipe <andresfppy@gmail.com>
2024-02-04 01:11:34 -0500

    Corregir la clase Schedule

--- src/main/java/com/app/reservasR/domain/entity/Schedule.java
+++ src/main/java/com/app/reservasR/domain/entity/Schedule.java
@@ -1,5 +1,6 @@
 package com.app.reservasR.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,7 +18,14 @@ public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private Date startTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private Date endTime;
+
+    @ManyToOne
+    private Restaurant restaurant;
 
 }
