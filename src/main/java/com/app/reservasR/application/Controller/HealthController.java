680f41ca01ed48a39cd3f308a3fb2dd43694d434
Felipe <andresfppy@gmail.com>
2024-02-05 22:52:25 -0500

    Agregar record HealthController

---
+++ src/main/java/com/app/reservasR/application/Controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.app.reservasR.application.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public record HealthController() {
+
+    @GetMapping
+    public ResponseEntity<?> healthApi(){
+        return new ResponseEntity<>("API working OK!", HttpStatus.OK);
+    }
+}
