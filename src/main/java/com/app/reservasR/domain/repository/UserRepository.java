ac57c7909bc1e4ce6ad8db8e6e55b04ce8c905c7
Felipe <andresfppy@gmail.com>
2024-02-05 22:48:10 -0500

    Agregar método findUserByEmail en UserRepository

--- src/main/java/com/app/reservasR/domain/repository/UserRepository.java
+++ src/main/java/com/app/reservasR/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.app.reservasR.domain.repository;
 import com.app.reservasR.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findUserByEmail(String email);
 }
